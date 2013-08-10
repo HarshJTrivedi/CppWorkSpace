1a7916db657a242884f8f6251820f201bed8edc7
HarshJTrivedi <harshworks94@gmail.com>
2013-08-10 10:14:04 -0700

    Initial Commit

---
+++ Lab1/include/LinkedList.h
@@ -0,0 +1,13 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+
+class LinkedList
+{
+    public:
+        LinkedList();
+    protected:
+    private:
+};
+
+#endif // LINKEDLIST_H
---
+++ Lab1/include/Node.h
@@ -0,0 +1,13 @@
+#ifndef NODE_H
+#define NODE_H
+
+
+class Node
+{
+    public:
+        Node();
+    protected:
+    private:
+};
+
+#endif // NODE_H
